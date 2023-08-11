2be8cf5ca0409f566aecd27973e2c7f09930497c
sekhubede <43635891+sekhubede@users.noreply.github.com>
2023-08-11 09:35:54 +0200

    Add regions to hold the logic for ref and value types

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+#region Value Types
+
+
+
+#endregion
+
+
+
+#region Reference Types
+
+
+
+#endregion
